53d757e8245e82d9f823b805f0ffe7626053d05d
rama4zis <rama.4ditya3@gmail.com>
2017-06-10 16:05:52 +0700

    now

--- js/main.js
+++ js/main.js
@@ -5,21 +5,21 @@ var bagian2 = document.getElementById('bagian2');
 bagian1.addEventListener("mouseenter", mouseMasuk);
 bagian1.addEventListener("mouseleave", mouseKeluar);
 function mouseMasuk(){
-  bagian1.style.backgroundColor = "#000";
+  bagian1.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
   bagian1.style.color           = "#fff";
   bagian1.style.transition = "1s";
 }
 function mouseKeluar(){
-  bagian1.style.backgroundColor = "#fff";
+  bagian1.style.backgroundColor = "initial";
   bagian1.style.color           = "#000";
 }
 
 bagian2.addEventListener('mouseenter', function(){
-  bagian2.style.backgroundColor = "#ea756b";
+  bagian2.style.backgroundColor = "rgba(235, 117, 107, 0.75)";
   bagian2.style.color = "#fff";
   bagian2.style.transition = "1s";
 });
 bagian2.addEventListener('mouseleave', function(){
-  bagian2.style.backgroundColor = "#fff";
+  bagian2.style.backgroundColor = "initial";
   bagian2.style.color = "#000";
 });
