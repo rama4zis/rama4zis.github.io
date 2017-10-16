b5ecfd08e83bcc6c807da12ed20b6d9b6fe37174
unknown <rama4zis>
2017-10-16 08:44:57 +0700

    Pemindahan pertama

---
+++ dasar/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Profile</title>
+
+    <link href="style/main.css" type="text/css" rel="stylesheet">
+    <link href="style/parallax.css" type="text/css" rel="stylesheet">
+    <link href="style/section.css" type="text/css" rel="stylesheet">
+    <link  href="style/table.css" type="text/css" rel="stylesheet">
+    <link  href="style/footer.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="header-nav">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#tentang">Tentang Saya</a></li>
+        <li><a href="#skill">Skill</a></li>
+        <li><a href="kontak.php">Contact</a></li>
+        <li><a href="hasil.php">Hasil Belajar</a></li>
+        <li style="float:right; "><a href="login.php"><b>Login</b></a></li>
+      </ul>
+    </nav>
